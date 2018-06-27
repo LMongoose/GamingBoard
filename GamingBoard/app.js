90118e24211f26da179a1d24e0658e4d10a85cf4
LMongoose <33644529+LMongoose@users.noreply.github.com>
2018-06-26 22:24:50 -0300

    Add files via upload

---
+++ GamingBoard/app.js
@@ -0,0 +1,149 @@
+var bodyParser = require("body-parser");
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var app = express();
+
+var SteamApi = require("steam-api");
+var apikey = "95E0B94D7BAE2D74B032FCA88F5E286A"
+var user = new SteamApi.User(apikey);
+var userStats = new SteamApi.UserStats(apikey);
+var player = new SteamApi.Player(apikey);
+
+// Define o Jogador e o Jogo
+var playerId = "76561198120299407"
+var appId = "620"
+
+// Cria Conexão com o Banco 
+mongoose.connect("mongodb://<gamingboard>:<178%4jkls>@ds117431.mlab.com:17431/gamingboard", { useMongoClient: true });
+
+// Verifica conexão com MongoDB
+var dbMongo = mongoose.connection;
+dbMongo.on("error", console.error.bind(console, "Não foi possível se conectar no MongoDB!"));
+dbMongo.once("open", function(){
+	console.log("Aplicação conectada no MongoDB");
+});
+
+// Cria um modelo baseado em um schema (http://mongoosejs.com/docs/guide.html) do Mongoose
+// Tipos podem ser definidos http://mongoosejs.com/docs/schematypes.html
+var Usuario = mongoose.model("users",
+	new mongoose.Schema({
+		username: { type: String, required: [true, "O nome de usuário é obrigatório!"]},
+		password: { type: String, required: [true, "A senha é obrigatória!"]},
+		email: { type: String, required: [false]}
+	})
+);
+
+// Configuração do Renderizador de Páginas (EJS)
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
+
+// Rotas
+app.get("/", function(req, res){
+   res.render("login")
+});
+
+app.get("/login", function(req, res){
+	res.render("login")
+ });
+
+app.get("/newuser", function(req, res){
+	res.render("newuser")
+});
+
+app.post("/dash", function(req, res){
+	res.render("dash")
+});
+
+app.get("/dash/jogador/level", function(req, res){
+	player.GetSteamLevel(playerId).done(function(result){
+		res.send("O nível do jogador " +playerId+ " é " +result);
+	});
+})
+
+app.get("/dash/jogador/amigos", function(req, res){
+	user.GetFriendList(optionalRelationship = "all", playerId).done(function(result){
+		res.send(result)
+	});
+})
+
+app.get("/dash/jogador/grupos", function(req, res){
+	user.GetUserGroupList(playerId).done(function(result){
+		res.send(result);
+	});
+})
+
+app.get("/dash/jogador/medalhas", function(req, res){
+	player.GetBadges(playerId).done(function(result){
+		res.send(result);
+	});
+})
+
+app.get("/dash/jogador/conquistas", function(req, res){
+	userStats.GetPlayerAchievements(appId, playerId).done(function(result){
+		res.send(result);
+	});
+})
+
+app.get("/dash/jogador/estatisticas", function(req, res){
+	userStats.GetUserStatsForGame(appId, playerId).done(function(result){
+		res.send(result);
+	});
+})
+
+app.get("/dash/jogo/jogadores", function(req, res){
+	userStats.GetNumberOfCurrentPlayers(appId).done(function(result){
+		res.send("Number of Current Players: " +result);
+	});
+})
+
+app.get("/dash/jogo/detalhes", function(req, res){
+	userStats.GetSchemaForGame(appId).done(function(result){
+		res.send(result);
+	});
+})
+
+app.get("/dash/jogo/porcentagemGlobal", function(req, res){
+	userStats.GetGlobalAchievementPercentagesForApp(appId).done(function(result){
+		res.send(result);
+	});
+})
+
+app.get("/dash/jogo/estatisticas", function(req, res){
+	appId = 17740;
+	statsName = ["global.map.emp_isle"];
+	userStats.GetGlobalStatsForGame(appId, statsName).done(function(result){
+		res.send(result);
+	});
+})
+
+// BANCO
+app.post("/create", function(req, res){
+	var username = req.body.usr
+	var email = req.body.email
+	var password = req.body.pwd
+
+	new Usuario({"username": username, "email": email, "password": password}).save(function(err, obj){
+		if (err){
+			console.error(err)
+			res.status(500).send("Erro na aplicação: " + err.message)
+		} else {
+			console.log("Usuário Adicionado!")
+			res.redirect("/")
+		}
+	})
+});
+
+app.set("port", (process.env.PORT || 3000));
+app.listen(app.get("port"), function(){
+	console.log("Servidor Inicializado na Porta", app.get("port"));
+});
+
+module.exports = app;
---
+++ SteamAPI-teste/steamApiExample.js
@@ -0,0 +1,121 @@
+﻿var SteamApi = require("steam-api");
+
+var apikey = "95E0B94D7BAE2D74B032FCA88F5E286A"
+
+var user = new SteamApi.User(apikey, optionalSteamId);
+var userStats = new SteamApi.UserStats(apikey, optionalSteamId);
+var news = new SteamApi.News(apikey);
+var app = new SteamApi.App(apikey);
+var player = new SteamApi.Player(apikey, optionalSteamId);
+var inventory = new SteamApi.Inventory(apikey, optionalSteamId);
+var items = new SteamApi.Items(apikey, optionalSteamId);
+
+// Steam API Backpack
+items.GetPlayerItems(appId, optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+// Inventory
+inventory.GetAppItems(appId, optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+// User methods
+user.GetPlayerBans(optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+user.GetFriendList(optionalRelationship = "all", optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+user.GetUserGroupList(optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+//// e.g. vanityUrl = "pr00fgames";
+user.ResolveVanityUrl(vanityUrl).done(function(result){
+	console.log(result);
+});
+
+
+// UserStats methods
+//// e.g. appId = 17740;
+//// e.g. statsName = ["global.map.emp_isle"];
+userStats.GetGlobalStatsForGame(appId, statsName).done(function(result){
+	console.log(result);
+});
+
+//// e.g. appId = 620;
+userStats.GetNumberOfCurrentPlayers(appId).done(function(result){
+	console.log(result);
+});
+
+userStats.GetSchemaForGame(appId).done(function(result){
+	console.log(result);
+});
+
+userStats.GetPlayerAchievements(appId, optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+userStats.GetGlobalAchievementPercentagesForApp(appId).done(function(result){
+	console.log(result);
+});
+
+userStats.GetUserStatsForGame(appId, optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+
+// News Methods
+news.GetNewsForApp(appId,
+				optionalCount = 5,
+				optionalMaxLength = null
+				).done(function(result){
+	console.log(result);
+});
+
+
+// App Methods
+app.appDetails(appId).done(function(result){
+	console.log(result);
+});
+
+app.GetAppList().done(function(result){
+	console.log(result);
+});
+
+app.GetServersAtAddress(addressOrIp).done(function(result){
+	console.log(result);
+});
+
+app.UpToDateCheck(appId, version).done(function(result){
+	console.log(result);
+});
+
+
+// Player Methods
+player.GetSteamLevel(optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+player.GetPlayerLevelDetails(optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+player.GetBadges(optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+player.GetCommunityBadgeProgress(optionalBadgeId, optionalSteamId).done(function(result){
+	console.log(result);
+});
+
+player.GetOwnedGames(optionalSteamId, 
+					optionalIncludeAppInfo = true, 
+					optionalIncludePlayedFreeGames = false, 
+					optionalAppIdsFilter = []
+					).done(function(result){
+	console.log(result);
+});
